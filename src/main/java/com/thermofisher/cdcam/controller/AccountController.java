b27b841ac04eac570d04328e6df2d499cba23eff
Ortiz, David A <david.ortiz@thermofisher.com>
2019-08-29 12:21:31 -0700

    Existing Account Validation

--- src/main/java/com/thermofisher/cdcam/controller/AccountController.java
+++ src/main/java/com/thermofisher/cdcam/controller/AccountController.java
@@ -46,37 +46,40 @@ public class AccountController {
             if (hashValidationService.isValidHash(hashValidationService.getHashedString(rawBody), headerValue)) {
                 JSONParser parser = new JSONParser();
                 JSONObject mainObject = (JSONObject) parser.parse(rawBody);
-                JSONArray events = (JSONArray)mainObject.get("events");
+                JSONArray events = (JSONArray) mainObject.get("events");
                 for (Object singleEvent : events) {
                     JSONObject event = (JSONObject) singleEvent;
                     JSONObject data = (JSONObject) event.get("data");
                     if (event.get("type").equals(Events.REGISTRATION.getValue())) {
                         String uid = data.get("uid").toString();
                         AccountInfo account = accounts.getAccount(uid);
-                        if ((account.getLoginProvider()).toLowerCase().contains(FederationProviders.OIDC.getValue()) || (account.getLoginProvider()).toLowerCase().contains(FederationProviders.SAML.getValue())) {
-                            ObjectMapper mapper = new ObjectMapper();
-                            String jsonString = mapper.writeValueAsString(account);
-                            if (snsHandler.sendSNSNotification(jsonString)) {
-                                logger.info("User sent to SNS");
-                                return new ResponseEntity<>(jsonString, HttpStatus.OK);
+                        if (account != null) {
+                            if ((account.getLoginProvider()).toLowerCase().contains(FederationProviders.OIDC.getValue()) || (account.getLoginProvider()).toLowerCase().contains(FederationProviders.SAML.getValue())) {
+                                ObjectMapper mapper = new ObjectMapper();
+                                String jsonString = mapper.writeValueAsString(account);
+                                if (snsHandler.sendSNSNotification(jsonString)) {
+                                    logger.info("User sent to SNS");
+                                    return new ResponseEntity<>(jsonString, HttpStatus.OK);
+                                } else {
+                                    logger.error("The user was not created through federation");
+                                    return new ResponseEntity<>("Something went wrong... An SNS Notification failed to be sent.", HttpStatus.SERVICE_UNAVAILABLE);
+                                }
                             } else {
                                 logger.error("The user was not created through federation");
-                                return new ResponseEntity<>("Something went wrong... An SNS Notification failed to be sent.", HttpStatus.SERVICE_UNAVAILABLE);
+                                return new ResponseEntity<>("The user was not created through federation", HttpStatus.OK);
                             }
                         } else {
                             logger.error("The user was not created through federation");
-                            return new ResponseEntity<>("The user was not created through federation", HttpStatus.OK);
+                            return new ResponseEntity<>("NO USER FOUND", HttpStatus.BAD_REQUEST);
                         }
-                    }
-                    else {
+                    } else {
                         logger.error("The event type was not recognized");
                         return new ResponseEntity<>("the event type was not recognized", HttpStatus.OK);
                     }
                 }
                 logger.error("NO EVENT FOUND");
                 return new ResponseEntity<>("NO EVENT FOUND", HttpStatus.BAD_REQUEST);
-            }
-            else{
+            } else {
                 logger.error("INVALID SIGNATURE");
                 return new ResponseEntity<>("INVALID SIGNATURE", HttpStatus.BAD_REQUEST);
             }
