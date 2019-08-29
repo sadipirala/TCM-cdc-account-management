4f90f2b03497444dd3266b11457a5fb0481139e3
Ortiz, David A <david.ortiz@thermofisher.com>
2019-08-29 15:53:01 -0700

    add specific errors

--- src/main/java/com/thermofisher/cdcam/aws/SecretsManager.java
+++ src/main/java/com/thermofisher/cdcam/aws/SecretsManager.java
@@ -3,8 +3,7 @@ package com.thermofisher.cdcam.aws;
 import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.amazonaws.services.secretsmanager.model.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,12 +20,31 @@ public class SecretsManager {
                 .build();
         GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
                 .withSecretId(secretName);
-        GetSecretValueResult getSecretValueResult;
+        GetSecretValueResult getSecretValueResult = null;
         try {
             getSecretValueResult = client.getSecretValue(getSecretValueRequest);
-        }catch (Exception e){
+        } catch (DecryptionFailureException e) {
+            // Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InternalServiceErrorException e) {
+            // An error occurred on the server side.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InvalidParameterException e) {
+            // You provided an invalid value for a parameter.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (InvalidRequestException e) {
+            // You provided a parameter value that is not valid for the current state of the resource.
+            // Deal with the exception here, and/or rethrow at your discretion.
+            throw e;
+        } catch (ResourceNotFoundException e) {
+            // We can't find the resource that you asked for.
+            // Deal with the exception here, and/or rethrow at your discretion.
             throw e;
         }
+
         if (getSecretValueResult.getSecretString() != null) {
             return getSecretValueResult.getSecretString();
         }
