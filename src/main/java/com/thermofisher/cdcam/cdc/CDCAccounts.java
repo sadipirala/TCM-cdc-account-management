d0f55ff21588a0afa8341d1946466680978ce8f0
Chacón, Maria Guadalupe <mariaguadalupe.chacon@thermofisher.com>
2019-10-03 10:57:13 -0700

    Created and implemented secrets for CDC user key and secret key.

--- src/main/java/com/thermofisher/cdcam/cdc/CDCAccounts.java
+++ src/main/java/com/thermofisher/cdcam/cdc/CDCAccounts.java
@@ -3,11 +3,16 @@ package com.thermofisher.cdcam.cdc;
 import com.gigya.socialize.GSObject;
 import com.gigya.socialize.GSRequest;
 import com.gigya.socialize.GSResponse;
+import com.thermofisher.cdcam.aws.SecretsManager;
 import com.thermofisher.cdcam.builders.AccountBuilder;
 import com.thermofisher.cdcam.enums.cdc.APIMethods;
 import com.thermofisher.cdcam.model.AccountInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import java.io.PrintWriter;
@@ -21,18 +26,21 @@ public class CDCAccounts {
     @Value("${cdc.apiKey}")
     private String apiKey;
 
-    @Value("${cdc.secretKey}")
-    private String secretKey;
+    @Value("${cdc.credentials}")
+    private String cdcKey;
 
-    @Value("${cdc.userKey}")
-    private String userKey;
+    private String[] credentials;
+
+    @Autowired
+    SecretsManager secretsManager;
 
     AccountBuilder accountBuilder = new AccountBuilder();
 
     public AccountInfo getAccount(String UID) {
         try {
+            credentials = getCredentials();
             String apiMethod = APIMethods.GET.getValue();
-            GSRequest request = new GSRequest(apiKey, secretKey, apiMethod, null, true, userKey);
+            GSRequest request = new GSRequest(apiKey, credentials[0], apiMethod, null, true, credentials[1]);
             request.setParam("UID", UID);
             request.setParam("include", "emails, profile, data, password,userInfo,regSource,identities");
             request.setParam("extraProfileFields", "username, locale,work");
@@ -57,23 +65,32 @@ public class CDCAccounts {
     }
 
     public GSResponse setUserInfo(String uid, String data, String profile) {
-        String apiMethod = APIMethods.SETINFO.getValue();
-        GSRequest request = new GSRequest(apiKey, secretKey, apiMethod, null, true, userKey);
-        if (uid != null) request.setParam("UID", uid);
-        if (data != null) request.setParam("data", data);
-        if (profile != null) request.setParam("profile", profile);
-        return request.send();
+        try {
+            String apiMethod = APIMethods.SETINFO.getValue();
+            credentials = getCredentials();
+            GSRequest request = new GSRequest(apiKey, credentials[0], apiMethod, null, true, credentials[1]);
+            if (uid != null) request.setParam("UID", uid);
+            if (data != null) request.setParam("data", data);
+            if (profile != null) request.setParam("profile", profile);
+            return request.send();
+        }catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String stackTrace = sw.toString();
+            logger.error(stackTrace);
+            return null;
+        }
     }
     
     public GSResponse setLiteReg(String email) {
         try {
             String apiMethod = APIMethods.SETINFO.getValue();
-            GSRequest request = new GSRequest(apiKey, secretKey, apiMethod, null, true, userKey);
+            credentials = getCredentials();
+            GSRequest request = new GSRequest(apiKey, credentials[0], apiMethod, null, true, credentials[1]);
             request.setParam("regToken", getRegToken(true));
             request.setParam("profile", String.format("{\"email\":\"%s\"}", email));
-            GSResponse response = request.send();
-
-            return response;
+            return request.send();
         } catch (Exception e) {
             StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
@@ -85,21 +102,32 @@ public class CDCAccounts {
     }
 
     public GSResponse search(String query, String accountTypes) {
-        if(query == null) return null;
-        final boolean USE_HTTPS = true;
-        String apiMethod = APIMethods.SEARCH.getValue();
+        try {
+            if(query == null) return null;
+            final boolean USE_HTTPS = true;
+            String apiMethod = APIMethods.SEARCH.getValue();
+            credentials = getCredentials();
 
-        GSRequest request = new GSRequest(apiKey, secretKey, apiMethod, null, USE_HTTPS, userKey);
-        request.setParam("accountTypes", accountTypes);
-        request.setParam("query", query);
+            GSRequest request = new GSRequest(apiKey, credentials[0], apiMethod, null, USE_HTTPS, credentials[1]);
+            request.setParam("accountTypes", accountTypes);
+            request.setParam("query", query);
 
-        return request.send();
+            return request.send();
+        } catch (ParseException e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String stackTrace = sw.toString();
+            logger.error(stackTrace);
+            return null;
+        }
     }
 
     private String getRegToken(boolean isLite) {
         try {
             String apiMethod = APIMethods.INITREG.getValue();
-            GSRequest request = new GSRequest(apiKey, secretKey, apiMethod, null, true, userKey);
+            credentials = getCredentials();
+            GSRequest request = new GSRequest(apiKey, credentials[0], apiMethod, null, true, credentials[1]);
             request.setParam("isLite", isLite);
 
             GSResponse response = request.send();
@@ -117,5 +145,12 @@ public class CDCAccounts {
             logger.error(stackTrace);
             return null;
         }
+    }
+
+    private String[] getCredentials() throws ParseException {
+        JSONObject secretProperties = (JSONObject) new JSONParser().parse(secretsManager.getSecret(cdcKey));
+        String secretKey = secretsManager.getProperty(secretProperties, "secretKey");
+        String userKey = secretsManager.getProperty(secretProperties, "userKey");
+        return new String[] { secretKey, userKey };
     }
 }
