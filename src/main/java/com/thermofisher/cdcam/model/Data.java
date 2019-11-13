56d3fa1847a158f5a13f1fc84ced8c9462b3264f
Chacón, Maria Guadalupe <mariaguadalupe.chacon@thermofisher.com>
2019-11-12 17:34:41 -0800

    ARM-1858: Added JsonIgnoreProperties annotation to models.

--- src/main/java/com/thermofisher/cdcam/model/Data.java
+++ src/main/java/com/thermofisher/cdcam/model/Data.java
@@ -1,5 +1,6 @@
 package com.thermofisher.cdcam.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Data {
     private Thermofisher thermofisher;
 }
