faee9c28f25a0920bf102924831f07b44a197080
Gustavo Mungarro <gustavo.mungarro@thermofisher.com>
2019-11-13 11:01:26 -0800

    ARM-1888: Constructor tags added to some models.

--- src/main/java/com/thermofisher/cdcam/model/Data.java
+++ src/main/java/com/thermofisher/cdcam/model/Data.java
@@ -1,5 +1,8 @@
 package com.thermofisher.cdcam.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +10,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Data {
     private Thermofisher thermofisher;
 }
