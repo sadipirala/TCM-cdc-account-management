68c2eec1db624a66d0a2f709f32cb1bdff0b1002
Alejandro Juarez <alejandro.juarez@thermofisher.com>
2020-03-24 15:35:09 -0700

    ARM-2403 Fixed type and unused import

--- src/main/java/com/thermofisher/cdcam/utils/Utils.java
+++ src/main/java/com/thermofisher/cdcam/utils/Utils.java
@@ -8,13 +8,12 @@ import org.json.JSONObject;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import org.apache.logging.log4j.Logger;
 
 public class Utils {
-    public static String getAlphaNumericString(int lenght) {
+    public static String getAlphaNumericString(int length) {
         final String ALPHANUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
-        StringBuilder sb = new StringBuilder(lenght);
-        for (int i = 0; i < lenght; i++) {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
             int index = (int) (ALPHANUMERIC_STRING.length() * Math.random());
             sb.append(ALPHANUMERIC_STRING.charAt(index));
         }
