51dc43a9220d4020106028b7c28bac6d0f763728
Ortiz, David A <david.ortiz@thermofisher.com>
2020-08-19 14:18:28 -0700

    added json property and constructor properties

--- src/main/java/com/thermofisher/cdcam/model/Emails.java
+++ src/main/java/com/thermofisher/cdcam/model/Emails.java
@@ -1,10 +1,14 @@
 package com.thermofisher.cdcam.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Emails {
     private String[] verified;
     private String[] unverified;
