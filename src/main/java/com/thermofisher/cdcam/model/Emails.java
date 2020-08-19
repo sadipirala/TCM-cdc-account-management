ec0a5d69c343a10b8ccf3366a356ce593ed41053
Ortiz, David A <david.ortiz@thermofisher.com>
2020-08-19 14:58:34 -0700

    added Builder to emails

--- src/main/java/com/thermofisher/cdcam/model/Emails.java
+++ src/main/java/com/thermofisher/cdcam/model/Emails.java
@@ -1,15 +1,25 @@
 package com.thermofisher.cdcam.model;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
-@Builder
-@JsonIgnoreProperties(ignoreUnknown = true)
+@Setter
+@Builder(builderClassName = "EmailsBuilder", toBuilder = true)
+@JsonDeserialize(builder = Emails.EmailsBuilder.class)
 public class Emails {
     private List<String> verified;
     private List<String> unverified;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class EmailsBuilder {
+    }
 }
