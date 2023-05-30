13cc21e82ff2d39af511ce875e5d4a6e06de18b4
Alejandro Juárez <alejandro.juarez@thermofisher.com>
2023-05-30 14:00:03 -0700

    BLR-1709 -> BLR-1765
    Fixed issue when autowiring static properties/methods

--- src/main/java/com/thermofisher/cdcam/properties/EmailVerificationProperties.java
+++ src/main/java/com/thermofisher/cdcam/properties/EmailVerificationProperties.java
@@ -1,6 +1,7 @@
 package com.thermofisher.cdcam.properties;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -13,35 +14,45 @@ public class EmailVerificationProperties {
     * A boolean that indicates whether the Email Verification feature should be disabled globally.
      */
     @Getter
-    @Value("${email-verification.enabled}")
-    private static boolean isEnabled;
+    private static boolean enabled;
 
     /**
      * A boolean that indicates whether the Email Verification feature should be enabled globally.
      */
     @Getter
-    @Value("${email-verification.global}")
-    private static boolean isGlobal;
+    private static boolean global;
 
     /**
      * Property used in data.verifiedEmailDate for accounts that should not need to go through the
      * Email Verification flow.
      */
     @Getter
-    @Value("${email-verification.defaultVerificationDate}")
     private static String defaultVerificationDate;
 
     /**
      * List of country codes that should prompt accounts to go through the Email Verification flow.
      */
     @Getter
-    @Value("${email-verification.includedCountries}")
     private static List<String> includedCountries;
 
     /**
      * List of country codes that should be excluded from the Email Verification flow.
      */
     @Getter
-    @Value("${email-verification.excludedCountries}")
     private static List<String> excludedCountries;
+
+    @Autowired
+    public EmailVerificationProperties(
+            @Value("${email-verification.enabled}") boolean enabled,
+            @Value("${email-verification.global}") boolean global,
+            @Value("${email-verification.defaultVerificationDate}") String defaultVerificationDate,
+            @Value("${email-verification.includedCountries}") List<String> includedCountries,
+            @Value("${email-verification.excludedCountries}") List<String> excludedCountries
+    ) {
+        EmailVerificationProperties.enabled = enabled;
+        EmailVerificationProperties.global = global;
+        EmailVerificationProperties.defaultVerificationDate = defaultVerificationDate;
+        EmailVerificationProperties.includedCountries = includedCountries;
+        EmailVerificationProperties.excludedCountries = excludedCountries;
+    }
 }
