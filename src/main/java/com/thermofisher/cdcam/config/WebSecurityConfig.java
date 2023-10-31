ba035b4f53e2324450c7bfe595ac33e8d45426ef
kavitha-kademar <118893764+kavitha-kademar@users.noreply.github.com>
2023-10-31 17:29:28 +0530

    Add authentication for swagger

--- src/main/java/com/thermofisher/cdcam/config/WebSecurityConfig.java
+++ src/main/java/com/thermofisher/cdcam/config/WebSecurityConfig.java
@@ -1,11 +1,17 @@
 package com.thermofisher.cdcam.config;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,6 +20,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
+    private static final String[] REQ_AUTHENTICATION = {
+            "/api-docs/**",
+            "/swagger-resources/**",
+            "/swagger-ui/**",
+            "/swagger-ui.html",
+            "/swagger/**"
+    };
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // Configure AuthenticationManagerBuilder
@@ -22,11 +35,28 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
 
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/**","/swagger-ui/**", "/v3/api-docs").permitAll())
+                        .requestMatchers(REQ_AUTHENTICATION).authenticated()
+                        .requestMatchers("/**").permitAll())
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(withDefaults()).build();
 
     }
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("test")
+                .password(passwordEncoder().
+                        encode(new String(new Base64().decode("dGhlcm1vZmlzaGVyQGFwaQ=="))))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+
+    }
+
 
 }
